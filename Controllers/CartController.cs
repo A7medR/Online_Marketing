a92ac6670d400e2c58bda96939832964b88d37d4
A7medR <ahmedrefaey1999@gmail.com>
2021-07-14 02:05:27 +0200

    Full project

---
+++ Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CartController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Cart
+
+        public ActionResult Index()
+        {
+            var products = db.products.Include(u => u.category).ToList();
+
+            return View(products);
+        }
+        public ActionResult viewCart()
+        {
+            var products = db.carts.ToList();
+
+            return PartialView("~/Views/Cart/_viewCart.cshtml",products);
+        }
+
+
+        public ActionResult AddToCart(int id)
+        {
+            //retrieve the product from database
+            var addproduct = db.products.Single(product => product.id == id);
+            //add it to shopping cart 
+            Cart cart = new Cart
+            {
+                product_id = id,
+                Product = db.products.SingleOrDefault(p => p.id == id),
+                added_at = DateTime.Now,
+            };
+
+            db.carts.Add(cart);
+            db.SaveChanges();
+            DateTime newdate = DateTime.Now;
+            TimeSpan ts = newdate - cart.added_at;
+            db.Entry(cart).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Product");
+        }
+
+
+        public ActionResult RemoveLine(int id)
+        {
+            var removeproduct = db.carts.Single(Cart=> Cart.product_id == id);
+            db.carts.Remove(removeproduct);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Product");
+        }
+
+    }
+
+
+}
---
+++ Models/Cart.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace WebApplication2.Models
+    {
+        public class Cart
+        {
+            [Key]
+            [ForeignKey("Product")]
+            public int product_id { get; set; }
+            public DateTime added_at { get; set; }
+            public virtual Product Product { get; set; }
+
+            public string PassedDate
+            {
+                get
+                {
+                    return AppManager.calculateDateDiff(added_at, DateTime.Now);
+                }
+            }
+        }
+
+        class AppManager
+        {
+            public static string calculateDateDiff(DateTime d1, DateTime d2)
+            {
+                var dateDif = d2.Subtract(d1);
+                if (dateDif.TotalHours < 1)
+                {
+                    return Convert.ToInt32(dateDif.TotalMinutes) + " Minutes ago";
+
+                }
+
+                else if (dateDif.TotalDays < 1)
+                {
+                    return Convert.ToInt32(dateDif.TotalHours) + " Hours ago";
+                }
+                else if (dateDif.TotalDays > 1 && dateDif.TotalDays < 7)
+                {
+                    return Convert.ToInt32(dateDif.TotalDays) + " Days  ago";
+
+                }
+                else if (dateDif.TotalDays >= 7)
+                {
+                    return Convert.ToInt32((dateDif.TotalDays / 7.0)) + " Weeks ago";
+
+                }
+                return "";
+            }
+        }
+    }
+
